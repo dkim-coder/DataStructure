65c1f00e2a13d4c0452e1b3678648cba87cad212
jeus5771 <jeus5771@naver.com>
2022-01-13 10:44:25 +0900

    삭제 2022-01-13

--- Hash/hash.py
+++ Hash/hash.py
@@ -1,5 +1,1 @@
-# Hash Table: List or Array (index 갖고 value 저장 가능)
-# 매우 빠른 평균 삽입, 삭제, 탐색 연산 제공 O(1)
-# key 값을 해시 함수를 활용해 index mapping -> hash_func(key) > return index
-# Hash function < 충돌이 적어야 좋다. > < 연산이 빨라야 한다. > tradeoff 관계
-# Collision resolution method (충돌회피방법)필요
+
