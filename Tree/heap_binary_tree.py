ecfd75a1b83cfba80793944b239944c6262891bc
jeus5771 <jeus5771@naver.com>
2022-01-14 12:55:53 +0900

    2022. 01. 14 heap 이진트리 v1.0.1

--- Tree/heap_binary_tree.py
+++ Tree/heap_binary_tree.py
@@ -8,25 +8,55 @@
 
 import random
 
+class HeapBinaryTree:
+    def __init__(self, tree: list = None):
+        self.tree = tree
+        self.size = len(tree)
+
+    def __str__(self):
+        return str(self.tree)
+
+    # heap 성질 이진 트리 만드는 함수
+    def make_heap(self):
+        # 끝 노드부터 올라오면서 heap성질 만족 여부 수정
+        for idx in range(self.size - 1, -1, -1):
+            self.heapify_down(idx)
+
+    # k노드 부터 자식 노드들 heap 만족하도록 수정 함수
+    def heapify_down(self, k):
+        while (k * 2 + 2) < self.size:  # 현재 노드가 leaf node가 아닌지 확인
+            l, r = 2 * k + 1, 2 * k + 2
+            m = self.tree.index(max(self.tree[k],self.tree[l],self.tree[r]))    # 최댓값 노드 index 구하기
+            if k != m:
+                self.tree[k], self.tree[m] = self.tree[m], self.tree[k]
+                k = m
+            else:
+                break
+
+    # 자식 노드와 부모노드 heap성질 비교해서 바꿔줌(key값 크기 비교)
+    def heapify_up(self, k):
+        while k > 0 and self.tree[(k - 1)//2] < self.tree[k]:
+            self.tree[k], self.tree[(k - 1)//2] = self.tree[(k - 1)//2], self.tree[k]
+            k = (k - 1)//2
+
+    # 최댓값 리턴
+    def find_max(self):
+        return self.tree[0]
+
+    # 최댓값 제거
+    def delete_max(self):
+        if self.size == 0:
+            return None
+        key = self.tree[0]
+        self.tree[0], self.tree[-1] = self.tree[-1], self.tree[0]
+        self.tree.pop()
+        self.heapify_down(0)
+        return key
+
 H = [random.randint(1, 100) for i in range(20)]   # heap 성질x 이진트리 가정
-
-# heap 성질 이진 트리 만드는 함수
-def make_heap(in_tree: list):
-    n = len(in_tree)
-    # 끝 노드부터 올라오면서 heap성질 만족 여부 수정
-    for idx in range(n-1, -1, -1):
-        heapify_down(in_tree, idx, n)
-
-# k노드 부터 자식 노드들 heap 만족하도록 수정 함수
-def heapify_down(A: list, k, n):
-    while (k * 2 + 2) < n:  # 현재 노드가 leaf node가 아닌지 확인
-        l, r = 2 * k + 1, 2 * k + 2
-        m = A.index(max(A[k],A[l],A[r]))    # 최댓값 노드 index 구하기
-        if k != m:
-            A[k], A[m] = A[m], A[k]
-            k = m
-        else:
-            break
-
-make_heap(H)
-print(H)
+h = HeapBinaryTree(H)
+h.make_heap()
+print(h)
+print(h.find_max())
+h.delete_max()
+print(h)
